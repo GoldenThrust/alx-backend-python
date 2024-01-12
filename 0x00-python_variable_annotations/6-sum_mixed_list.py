baa5fb7cf3d294c92db8bce99bbb45f49782c624
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-01-12 02:52:23 +0100

    Requirement Done

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ python variable anotation """
-from typing import Union
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ return the sum of mxd_lst """
     return sum(mxd_lst)
