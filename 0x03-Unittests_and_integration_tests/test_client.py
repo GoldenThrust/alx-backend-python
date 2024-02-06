6c8d233f68975ee7016cd316a06c6ed826fa87b2
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-06 01:31:07 +0100

    Testing

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -68,7 +68,7 @@ class TestGithubOrgClient(unittest.TestCase):
     )
     def test_has_license(
         self, repo: Dict[str, Dict], license_key: str, expected: bool
-    ) -> None:
+    ):
         """test has license"""
         result = GithubOrgClient.has_license(repo, license_key)
         self.assertEqual(result, expected)
